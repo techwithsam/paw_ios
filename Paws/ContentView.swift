d1127f14a67674dda321c10635a9f4998daeb63a
Samuel Adekunle <samuelbeebest@gmail.com>
2025-07-24 20:18:00 +0100

    Initial Commit

---
+++ Paws/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Paws
+//
+//  Created by Samuel Adekunle on 24/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Samuel Adekunle on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
